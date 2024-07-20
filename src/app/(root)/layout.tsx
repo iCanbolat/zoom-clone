c86f71ec96419d3ba16729325f26941d8afbe085
Fatih Canbolat <70484185+iCanbolat@users.noreply.github.com>
2024-07-20 22:40:11 +0300

    auth&meeting setups

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+import StreamVideoProvider from '@/providers/stream-client-provider';
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
